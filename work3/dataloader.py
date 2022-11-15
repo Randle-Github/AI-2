0067f44882e6682236fe5dd6023daab00f82f5c5
Randle <79912692+Randle-Github@users.noreply.github.com>
2022-11-15 20:49:50 +0800

    Update dataloader.py

--- work3/dataloader.py
+++ work3/dataloader.py
@@ -6,10 +6,14 @@ import re
 import random
 import os
 
+import cv2
 import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
 
 SOS_token = 0
 EOS_token = 1
@@ -122,10 +126,46 @@ def tensorsFromPair(pair):
     target_tensor = tensorFromSentence(output_lang, pair[1])
     return (input_tensor, target_tensor)
 
-input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
-
-class DATA(): # data类
-    pass
+# input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
 
 class Flicker8k(): # flicker8k类
-    pass
+    def __init__(self, cfg) -> None:
+        self.res = cfg["RESNET"]["ENABLE"] # 此处是否为resnet的预处理部分
+        self.img, self.cap = self.get_site() # 这里获得img和caption的地址以及描述
+        self.lang = Lang("caption")
+        self.get_sentence(self.cap)
+        if not self.res:
+            self.features = torch.load("data/features.pth") # 正式训练需要直接使用features来进行操作
+
+    def get_site(self):
+        f = open("/opt/data/private/liuyangcen/homework/work3/data/captions.txt")
+        img = []
+        cap = []
+        content = f.readlines()
+        for line in content[1:]:
+            line = line.replace('"', '')
+            line = line.rstrip("\n")
+            line = line.lower()
+            i, c = line.split(".jpg,")
+            img.append(i)
+            cap.append(c)
+        return img, cap
+
+    def get_sentence(self, cap):
+        for sentence in cap:
+            self.lang.addSentence(sentence)
+
+    def __getitem__(self, idx):
+        if self.res: # res模式需要输入原始的图像
+            img = cv2.imread("/opt/data/private/liuyangcen/homework/work3/data/Images/"+self.img[idx]+".jpg")
+            img = transforms.functional.to_tensor(cv2.resize(img, (224,224)))
+            return img, None
+        else: # 非res模式需要输入得到的特征
+            img = self.features[idx]
+            return img, self.cap[idx]
+
+    def __len__(self):
+        return len(self.img)
+
+if __name__ == "__main__":
+    t = Flicker8k()
