11200b9bc039d9ad9c4576e294d97940b03434b6
Randle <79912692+Randle-Github@users.noreply.github.com>
2022-11-15 20:53:24 +0800

    Update heads.py

--- work4/heads.py
+++ work4/heads.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 import math
+import cv2
 
 import torch
 from torch import nn
@@ -17,3 +18,18 @@ import numpy as np
 
 from torch.distributions import Normal, kl_divergence
 import random
+
+import os
+
+import argparse
+
+#from model_test import UNet_conditional
+from utils import sin_time_embeding, beta_schedule
+from torch.utils.data import DataLoader
+from torchvision.transforms.functional import pil_to_tensor
+from torchvision.transforms import ToPILImage
+import torchvision
+from torchsummary import summary
+from tqdm import tqdm
+import sys
+from PIL import Image
